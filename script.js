2216aa48c4939ef1a99a26ae3042df8c708c67b5
fugit85 <163546724+fugit85@users.noreply.github.com>
2025-03-08 09:33:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/*document.getElementById(`button`).onclick = function() {
+    let text = document.getElementById(`input`).value;
+    result = `"${text}"`;
+    document.getElementById(`result`).textContent = result;
+}*/
+
+document.getElementById(`button`).onclick = function() {
+    let text = document.getElementById(`input`).value;
+    let lines = text.split('\n');
+    let quotedLines = lines.map(line => `"${line}"`);
+    let result = quotedLines.map(line => line === '""' ? '' : line).join('\n');
+    document.getElementById(`result`).textContent = result;
+}
+
+document.getElementById(`copy`).onclick = function() {
+    let result = document.getElementById(`result`);
+    result.select();
+    document.execCommand(`copy`);
+    alert(`Copied to clipboard!`);
+}

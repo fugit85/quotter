2fe2356a46339a0f1c34177c3ad0ae561ccb03c1
fugit85 <163546724+fugit85@users.noreply.github.com>
2025-03-10 09:07:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-/*document.getElementById(`button`).onclick = function() {
-    let text = document.getElementById(`input`).value;
-    result = `"${text}"`;
-    document.getElementById(`result`).textContent = result;
-}*/
-
-document.getElementById(`button`).onclick = function() {
-    let text = document.getElementById(`input`).value;
-    let lines = text.split('\n');
-    let quotedLines = lines.map(line => `"${line}"`);
-    let result = quotedLines.map(line => line === '""' ? '' : line).join('\n');
-    document.getElementById(`result`).textContent = result;
+function setButtonClickHandler() {
+    let mode = document.getElementById(`mode`).value;
+    document.getElementById(`button`).onclick = function() {
+        let text = document.getElementById(`input`).value.trimEnd();
+        text = text.split('\n').map(line => line.trimStart().replace(/\s+/g, ' ')).join('\n');
+        let lines = text.split('\n');
+        let quotedLines = lines.map(line => {
+            if (mode === 'phrase') {
+                return `"${line}"`;
+            } else if (mode === 'exact') {
+                return `[${line}]`;
+            } else {
+                return line;
+            }
+        });
+        let result = quotedLines.filter(line => line !== '""' && line !== '[]' && line.trim() !== '').join('\n');
+        document.getElementById(`result`).textContent = result;
+    }
 }
+
+document.getElementById(`mode`).onchange = setButtonClickHandler;
+setButtonClickHandler();
 
 document.getElementById(`copy`).onclick = function() {
     let result = document.getElementById(`result`);
